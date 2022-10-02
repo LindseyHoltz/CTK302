4946c1143c77be8bfa5daeaa2c3c9c10837522ee
Lindsey Holtz <llholtz@ilstu.edu>
2022-10-02 17:17:39 -0500

    touches

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,7 +1,13 @@
 let numberOfTouches ;
+let i1, i2, i3; 
 
 function setup() {
   createCanvas(400, 400);
+  i1 = loadImage("assets/mydrink.jpeg") ;
+  i2 = loadImage("assets/spongebob.webp") ;
+  i3 = loadImage("assets/patrick.webp") ;
+  imageMode(CENTER);
+  textSize(15);
 }
 
 function draw() {
@@ -11,21 +17,24 @@ function draw() {
   
   switch(numberOfTouches) {
     case 0: 
-      text("", 5, 22) ; 
+      text("Touch the screen for a Spongebob meme.", 5, 22) ; 
       break ;
       
       case 1: 
-       text("it's kind of lonely here", 5, 22) ; 
+      image(i1, width/2, height/2, 200, 200);
+       text("Wow so great! Touch the screen with another finger for a new meme.", 5, 22) ; 
       // put a picture here
       break ;
       
       case 2:
-      text("two fingers are touching the screen", 5, 22) ; 
+      image(i2, width/2, height/2, 200, 200);
+      text("This is a good one! Press the screen with another finger for a new meme.", 5, 22) ; 
             // put a picture here
       break ;
       
       case 3:
-     text("three are touching the screen", 5, 22) ; 
+      image(i3, width/2, height/2, 200, 200);
+      text("Wow! This is the last meme!", 5, 22) ; 
             // put a picture here
       break ;
     
