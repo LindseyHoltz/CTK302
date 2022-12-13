15c0eca772b5c75419ec4b3d07656eb44197c9db
Lindsey Holtz <94629329+LindseyHoltz@users.noreply.github.com>
2022-12-12 22:56:06 -0600

    Add files via upload

--- Unit4/googledata/sketch.js
+++ Unit4/googledata/sketch.js
@@ -1,9 +1,20 @@
 var bubbles = [];
 let url = "";
 let x = 0; 
+let f1, f2; 
+let turtle; 
+let rabbit; 
+let grump; 
+let lucy; 
+let clouds; 
+let s1;
+
+function preload() {
+  s1 = loadSound("assets/lion.mp3"); 
+}
 
 function setup() {
-  let key = "1Vkk1BuFus4oCddL6CYJx8KtHDsCqyxUO0Xddgt7kt-A"; // this is KEY of the URL from the sheet
+  let key = "1idT9q1ur35kSE8R7iUl8b1VU9LoAnb3B8c3tAVmJ518"; // this is KEY of the URL from the sheet
   
   //  let key = "1g6o2AfpufqOBwo1bthRzXYd_S85WxsjYqWIU4t16rzg"; // this is KEY of the URL from the sheet
 
@@ -12,10 +23,24 @@ function setup() {
   loadJSON(url, gotData);
 
   // Regular setup code we usually have
-  createCanvas(600, 600);
+  createCanvas(900, 600);
   textAlign(CENTER);
   ellipseMode(CENTER);
   rectMode(CENTER);
+  imageMode(CENTER);
+
+  turtle = loadImage("assets/happyturtle.png");
+  rabbit = loadImage("assets/rabbit.png");
+  grump = loadImage("assets/cat3.png");
+  lucy = loadImage("assets/cat2.png");
+  clouds = loadImage("assets/clouds.png"); 
+
+  f1 = loadFont("assets/fridaybeach.ttf");
+  f2 = loadFont("assets/star.ttf");
+
+  s1.play();
+  s1.loop(); 
+
 }
 
 // The data comes back as an array of objects
@@ -28,16 +53,17 @@ function gotData(data) {
     bubbles.push(
       new Bubble(
         data[i]["What is your name?"],
-        data[i]["What is your favorite animal? "],
-        data[i]["DC or Marvel? "],
-        data[i]["Best SpongeBob Character? "])); // THESE NEED TO MATCH SPREADSHEET
+        data[i]["What is your favorite animal?"],
+        data[i]["DC or Marvel?"],
+        data[i]["Best SpongeBob Character?"])); // THESE NEED TO MATCH SPREADSHEET
     
   }
 }
 
 function draw() {
-  background("lightgreen");
-
+  //image(clouds, 600, 900, 300, 450);
+  background(212, 234, 250);
+  
   // // iterate through the bubbles and display the objects!
   for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].display();
@@ -53,26 +79,49 @@ class Bubble {
     this.hero = hero;
     this.spongebob = spongebob; 
     this.pos = createVector(random(width), random(height));
-    this.vel = createVector(random(2, 5), 0);
+    this.vel = createVector(random(-3, 3), random(-3, 3));
+    this.color = color(random(255), random(255), random(255)); 
+    this.width = random(200, 240); 
+    this.height = random(140, 180); 
   }
 
   display() {
+    image(lucy, this.pos.x, this.pos.y - 100, 125, 100); 
+    //image(grump, 100, 100, 125, 200); 
+    //image(turtle, 100, 100, 100, 100); 
+    image(rabbit, x - 350, 250, 140, 115); 
+
     push(); 
-    stroke("lightblue");
-    fill("pink");
-    rect(this.pos.x, this.pos.y+10, 180, 120);
+    fill("black"); 
+    textFont(f2, 50); 
+    text("CTK 302 Survey!", x, 300 ); 
+    x += 0.5;
+
+    if (x > width + 400) {
+      x = -1000;
+    }
+    pop(); 
+    push(); 
+    //stroke("lightblue");
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y+10, this.width, this.height);
     pop(); 
     fill("black");
+    textFont(f1, 25); 
     text(
       this.name + "\n" + this.animal + "\n" + this.hero + "\n" + this.spongebob,
       this.pos.x,
-      this.pos.y
+      this.pos.y - 30
     );
     
+    //this.pos.add(this.vel);
+    //if (this.pos.x > width) this.pos.x = -10; 
+  
     this.pos.add(this.vel);
-    if (this.pos.x > width) this.pos.x = -10; 
-  
-  
+    if (this.pos.x > width + 100) this.pos.x = 0 - 100;
+    if (this.pos.x < 0 - 100) this.pos.x = width + 100;
+    if (this.pos.y > height + 100) this.pos.y = 0 - 100;
+    if (this.pos.y < 0 - 100) this.pos.y = height + 100;
     
   }
   
